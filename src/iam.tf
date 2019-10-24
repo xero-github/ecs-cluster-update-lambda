5ea41b77c12ec6b38c9b65365bc5c883878dc19e
Gokulakrishnan Ramachandran <gokulakrishnan.r@xero.com>
2019-10-24 14:46:11 +1300

    Upgrade to terraform 12

--- src/iam.tf
+++ src/iam.tf
@@ -17,16 +17,16 @@ data "aws_iam_policy_document" "trust_policy" {
 ##############################################################
 resource "aws_iam_role" "drain_lambda" {
   name               = "${var.drain_lambda_name}-${var.region}"
-  assume_role_policy = "${data.aws_iam_policy_document.trust_policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.trust_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "basic_execution" {
-  role       = "${aws_iam_role.drain_lambda.name}"
+  role       = aws_iam_role.drain_lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "asg_notifications" {
-  role       = "${aws_iam_role.drain_lambda.name}"
+  role       = aws_iam_role.drain_lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AutoScalingNotificationAccessRole"
 }
 
@@ -35,7 +35,7 @@ resource "aws_iam_policy" "drain_lambda_permissions" {
   name        = "${aws_iam_role.drain_lambda.name}-permissions-${var.region}"
   description = "Allow all actions required to gracefully migrate ECS tasks"
   path        = "/"
-  policy      = "${data.aws_iam_policy_document.drain_lambda_permissions.json}"
+  policy      = data.aws_iam_policy_document.drain_lambda_permissions.json
 }
 
 data "aws_iam_policy_document" "drain_lambda_permissions" {
@@ -67,8 +67,8 @@ data "aws_iam_policy_document" "drain_lambda_permissions" {
 }
 
 resource "aws_iam_role_policy_attachment" "drain_lambda_permissions" {
-  role       = "${aws_iam_role.drain_lambda.name}"
-  policy_arn = "${aws_iam_policy.drain_lambda_permissions.arn}"
+  role       = aws_iam_role.drain_lambda.name
+  policy_arn = aws_iam_policy.drain_lambda_permissions.arn
 }
 
 ##############################################################
@@ -76,11 +76,11 @@ resource "aws_iam_role_policy_attachment" "drain_lambda_permissions" {
 ##############################################################
 resource "aws_iam_role" "tag_lambda" {
   name               = "${var.tag_lambda_name}-${var.region}"
-  assume_role_policy = "${data.aws_iam_policy_document.trust_policy.json}"
+  assume_role_policy = data.aws_iam_policy_document.trust_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "tag_lambda_basic_execution" {
-  role       = "${aws_iam_role.tag_lambda.name}"
+  role       = aws_iam_role.tag_lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
@@ -89,7 +89,7 @@ resource "aws_iam_policy" "tag_lambda_permissions" {
   name        = "${aws_iam_role.tag_lambda.name}-permissions-${var.region}"
   description = "Mark old ECS instances before rolling cluster update"
   path        = "/"
-  policy      = "${data.aws_iam_policy_document.tag_lambda_permissions.json}"
+  policy      = data.aws_iam_policy_document.tag_lambda_permissions.json
 }
 
 data "aws_iam_policy_document" "tag_lambda_permissions" {
@@ -108,6 +108,7 @@ data "aws_iam_policy_document" "tag_lambda_permissions" {
 }
 
 resource "aws_iam_role_policy_attachment" "tag_lambda_permissions" {
-  role       = "${aws_iam_role.tag_lambda.name}"
-  policy_arn = "${aws_iam_policy.tag_lambda_permissions.arn}"
+  role       = aws_iam_role.tag_lambda.name
+  policy_arn = aws_iam_policy.tag_lambda_permissions.arn
 }
+
--- src/lambda.tf
+++ src/lambda.tf
@@ -8,15 +8,15 @@ data "archive_file" "drain_lambda_zip" {
 }
 
 resource "aws_lambda_function" "drain_lambda" {
-  filename         = "${data.archive_file.drain_lambda_zip.output_path}"
-  source_code_hash = "${data.archive_file.drain_lambda_zip.output_base64sha256}"
-  role             = "${aws_iam_role.drain_lambda.arn}"
-  function_name    = "${var.drain_lambda_name}"
+  filename         = data.archive_file.drain_lambda_zip.output_path
+  source_code_hash = data.archive_file.drain_lambda_zip.output_base64sha256
+  role             = aws_iam_role.drain_lambda.arn
+  function_name    = var.drain_lambda_name
   description      = "Gracefully migrate ECS tasks from container instance about to be terminated"
   handler          = "drain.handler"
   runtime          = "python3.6"
   timeout          = 300
-  tags             = "${var.tags}"
+  tags             = var.tags
 }
 
 ##############################################################
@@ -29,13 +29,14 @@ data "archive_file" "tag_lambda_zip" {
 }
 
 resource "aws_lambda_function" "tag_lambda" {
-  filename         = "${data.archive_file.tag_lambda_zip.output_path}"
-  source_code_hash = "${data.archive_file.tag_lambda_zip.output_base64sha256}"
-  role             = "${aws_iam_role.tag_lambda.arn}"
-  function_name    = "${var.tag_lambda_name}"
+  filename         = data.archive_file.tag_lambda_zip.output_path
+  source_code_hash = data.archive_file.tag_lambda_zip.output_base64sha256
+  role             = aws_iam_role.tag_lambda.arn
+  function_name    = var.tag_lambda_name
   description      = "Mark old ECS instances before rolling cluster update"
   handler          = "tag.handler"
   runtime          = "python3.6"
   timeout          = 300
-  tags             = "${var.tags}"
+  tags             = var.tags
 }
+
